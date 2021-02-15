210990c9167535fabce15d9677944a5a093a5b74
Brynne98 <36798170+Brynne98@users.noreply.github.com>
2021-02-15 11:35:17 +0200

    First stage partials implementation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const path =  require("path");
 const express = require("express");
-
-const publicDir = path.join(__dirname, "../public");
+const hbs = require("hbs");
 
 const app = express();
 
+//Define paths for Express config
+const publicDir = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs');
-app.use(express.static(publicDir))
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
     res.render('index', {
+        title: 'My Website',
+        message: 'You can learn more about me here',
+        createdBy: 'Brynne Prince',
         name: 'Brynne Prince',
         position: 'Software developer'
     });
@@ -17,8 +29,18 @@ app.get('', (req, res) => {
 
 app.get("/help", (req, res) => {
     res.render("help", {
-        title: 'HELP',
-        message: 'How can I help you?'
+        title: 'Help',
+        message: 'How can I help you?',
+        createdBy: 'Brynne Prince',
+
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: 'About',
+        message: 'This is the about page',
+        createdBy: 'Brynne Prince'
     })
 })
 
@@ -29,7 +51,6 @@ app.get("/weather", (req, res) => {
         "location": "My home"
     })
 })
-
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
