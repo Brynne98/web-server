77a6648ac423618b4fe1b813b1013c23e93dd8e4
Brynne98 <36798170+Brynne98@users.noreply.github.com>
2021-02-16 12:00:27 +0200

    Added 404 page handling

--- src/app.js
+++ src/app.js
@@ -52,6 +52,22 @@ app.get("/weather", (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        createdBy: 'Brynne Prince',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        createdBy: 'Brynne Prince',
+        errorMessage: 'Page not found'
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 })
